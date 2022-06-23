269bdf8f7efe2203578f2fcc210f28ff4224dd9c
Sabry Mohamed <Sabry@pere.com>
2022-06-23 23:35:37 +0200

    [FF-1] converter panel UI

--- src/app/currency-exchanger/currency-exchanger.module.ts
+++ src/app/currency-exchanger/currency-exchanger.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CurrencyExchangerRoutingModule } from './currency-exchanger-routing.module';
 import { LandingComponent } from './landing/landing.component';
+import { ConverterComponent } from './converter/converter.component';
+import { MaterialsModule } from '../materials/materials.module';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    ConverterComponent
   ],
   imports: [
     CommonModule,
-    CurrencyExchangerRoutingModule
+    CurrencyExchangerRoutingModule,
+    MaterialsModule
   ]
 })
 export class CurrencyExchangerModule { }
